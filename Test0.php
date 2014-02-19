4af2be69661869a84572dd87a92c893c1cb85e15
Eric Klawitter <eklawitter1@comcast.net>
2014-02-19 14:29:22 -0500

    dropzone allows file upload using drag/click with no connection to server

---
+++ Test0.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title> Darkroom</title>
+         <style type="text/css">
+             .navbar-static-top {
+                margin-bottom: 20px;
+             }
+         </style>
+
+        <link rel="stylesheet" href="/DropZone/css/dropzonecss.css">
+		<script src="/Dropzone/js/dropzone.js"></script>
+		
+    </head>
+    <body>          
+            <div class="navbar navbar-static-top">
+  <div class="navbar-inner">
+    <div class="container">
+   
+      <div class="nav-collapse collapse">
+        <ul class="nav">
+        </ul>
+        <ul class="nav pull-right">
+                      <li class=""><a href="http://InsertRegistrationLink.com">Register</a></li>
+            <li class=""><a href="http://InsertLoginLink.com"><i class="icon-login"></i>Login</a></li>
+          
+        <ul>
+      </div>
+    </div>
+  </div>
+</div>
+
+            <div class="container">
+                <div class="row">
+                    <div class="span12">
+                                        </div>
+                </div>
+            </div>
+
+            <div class="container">
+                <div class="row">
+	<div class="span8 offset2">
+			
+		<div class="alert alert-info canbehidden">
+				
+					<p><strong>Welcome to Darkroom!</strong> <br></p>
+		</div>
+		
+		<div class="well well-small canbehidden"><p>Upload files by clicking on the box below or by dragging files into it</p></div>
+
+		<form accept-charset="utf-8" class="form-inline" id="shareform" method="POST"></form>
+		<form action="http://fldrp.me/buckets/n/y2sYPdT6" class="dropzone" id="filedrop"></form>
+	</div>
+</div>
+
+<script>(function(){var uv=document.createElement('script');uv.type='text/javascript';uv.async=true;uv.src='//widget.uservoice.com/tsMObyfPS3TFC5aAp6sqmg.js';var s=document.getElementsByTagName('script')[0];s.parentNode.insertBefore(uv,s)})()</script>
+
+</body>
+</html>
