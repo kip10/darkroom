f3622c7b6ace1af8af6390624a3e341300896b1c
Eric Klawitter <eklawitter1@comcast.net>
2014-03-17 21:11:14 -0400

    MainActivity now has code outline, just need to interact with DB

--- src/com/example/darkroom/MainActivity.java
+++ src/com/example/darkroom/MainActivity.java
@@ -2,14 +2,28 @@ package com.example.darkroom;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends Activity {
-
+public class MainActivity extends Activity{
+	EditText userNameText;
+	EditText passwordText;
+	Button loginButton;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		userNameText = (EditText)findViewById(R.id.editText1);
+		passwordText = (EditText)findViewById(R.id.editText2);
+	
+		loginButton = (Button)findViewById(R.id.button1);
+		loginButton.setOnClickListener((android.view.View.OnClickListener) this);
+		
 	}
 
 	@Override
@@ -18,5 +32,17 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+
+	public void onClick(View v) {
+		String name = userNameText.getText().toString();
+		String pass = passwordText.getText().toString();
+		
+		switch(v.getId()){
+		case R.id.button1:
+			//implement database connection
+			break;
+		}
+	}
+
 
 }
