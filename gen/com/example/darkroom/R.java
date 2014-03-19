6d5664979ac242f9fb6b0bb943ff138ebf654119
Eric Klawitter <eklawitter1@comcast.net>
2014-03-19 11:01:25 -0400

    added homeActivity class to go to after successful login

--- gen/com/example/darkroom/R.java
+++ gen/com/example/darkroom/R.java
@@ -26,15 +26,17 @@ public final class R {
     public static final class id {
         public static final int action_settings=0x7f080004;
         public static final int button1=0x7f080002;
-        public static final int editText1=0x7f080000;
-        public static final int editText2=0x7f080001;
-        public static final int editText3=0x7f080003;
+        public static final int passwordTextField=0x7f080001;
+        public static final int statusTextField=0x7f080003;
+        public static final int userTextField=0x7f080000;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_home=0x7f030000;
+        public static final int activity_main=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int home=0x7f070000;
+        public static final int main=0x7f070001;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
@@ -42,6 +44,7 @@ public final class R {
         public static final int hello_world=0x7f050002;
         public static final int password=0x7f050004;
         public static final int register=0x7f050005;
+        public static final int title_activity_home=0x7f050006;
         public static final int userName=0x7f050003;
     }
     public static final class style {
--- src/com/example/darkroom/MainActivity.java
+++ src/com/example/darkroom/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
+import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
@@ -22,12 +23,44 @@ public class MainActivity extends Activity{
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		userNameText = (EditText)findViewById(R.id.editText1);
-		passwordText = (EditText)findViewById(R.id.editText2);
-		errorText = (EditText)findViewById(R.id.editText3);
+		userNameText = (EditText)findViewById(R.id.userTextField);
+		passwordText = (EditText)findViewById(R.id.passwordTextField);
+		errorText = (EditText)findViewById(R.id.statusTextField);
 		
 		loginButton = (Button)findViewById(R.id.button1);
-		loginButton.setOnClickListener((android.view.View.OnClickListener) this);
+		loginButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) { // go to next screen
+				String name = userNameText.getText().toString();
+				String pass = passwordText.getText().toString();
+				
+				switch(v.getId()){
+				case R.id.button1:
+					try {
+						ResultSet users = DatabaseQueryer.connectToAndQueryDatabase("SELECT * FROM users u WHERE u.username = " + name + "AND u.password = " + pass);
+						if(users.getMetaData().getColumnCount() != 1){
+							errorText.setText("Error: Incorrect Username/Password");
+							passwordText.setText("");
+						}
+						else{
+							Intent goToHomePage = new Intent(v.getContext(), HomeActivity.class);
+							startActivityForResult(goToHomePage, 0);
+							
+						}
+					} catch (SQLException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					
+					break;
+					//implement any other buttons needed
+				} // end switch
+			}
+
+				
+			
+		});
 		
 	}
 
@@ -38,30 +71,7 @@ public class MainActivity extends Activity{
 		return true;
 	}
 
-	public void onClick(View v) {
-		String name = userNameText.getText().toString();
-		String pass = passwordText.getText().toString();
-		
-		switch(v.getId()){
-		case R.id.button1:
-			try {
-				ResultSet users = DatabaseQueryer.connectToAndQueryDatabase("SELECT * FROM users u WHERE u.username = " + name + "AND u.password = " + pass);
-				if(users.getMetaData().getColumnCount() != 1){
-					errorText.setText("Error: Incorrect Username/Password");
-					passwordText.setText("");
-				}
-				else{
-					//TODO go to next screen
-				}
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
-			break;
-			//implement any other buttons needed
-		} // end switch
-	}
 
+		
 
 }
