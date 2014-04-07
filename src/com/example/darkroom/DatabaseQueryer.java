d985d3b549b0562a0215ad1ad59f3c3a89faa36d
Eric Klawitter <eklawitter1@comcast.net>
2014-04-06 22:52:35 -0400

    changed database connection to connect to my database

--- src/com/example/darkroom/DatabaseQueryer.java
+++ src/com/example/darkroom/DatabaseQueryer.java
@@ -8,15 +8,15 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class DatabaseQueryer {
-		private static String hostName = "54.201.38.30";
+		private static String hostName = "31.170.160.98";
 		private static Integer portNumber = 3306;
-		private static String userName = "admin";
-		private static String password = "12345678";
+		private static String userName = "a2884703_klaw";
+		private static String password = "Eklawitter1";
 
 		public static ResultSet connectToAndQueryDatabase(String query) throws SQLException {
 			Connection dbConn;
 			dbConn = DriverManager.getConnection(
-					"jdbc:mysql://" + hostName + ":" + portNumber + "/databaseproject?user=" + userName + "&password=" + password);
+					"jdbc:mysql://" + hostName + ":" + portNumber + "/a2884703_darkroo?user=" + userName + "&password=" + password);
 			Statement stmt = dbConn.createStatement();
 			return stmt.executeQuery(query);
 		}
@@ -25,7 +25,7 @@ public class DatabaseQueryer {
 		
 		public static void connectToAndUpdateDatabase(String statement) throws SQLException {
 			Connection dbCon = DriverManager.getConnection(
-					"jdbc:mysql://" + hostName + ":" + portNumber + "/databaseproject?user=" + userName + "&password=" + password);
+					"jdbc:mysql://" + hostName + ":" + portNumber + "/a2884703_darkroo?user=" + userName + "&password=" + password);
 			Statement stmt = dbCon.createStatement();
 			stmt.executeUpdate(statement);
 		}
