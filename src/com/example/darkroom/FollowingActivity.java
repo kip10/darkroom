d03e3d17ebb00c194d869180ee197af340ca0385
Eric Klawitter <eklawitter1@comcast.net>
2014-04-17 10:34:09 -0400

    added tabview to the userActivity screen

---
+++ src/com/example/darkroom/FollowingActivity.java
@@ -0,0 +1,14 @@
+package com.example.darkroom;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class FollowingActivity extends Activity{
+	@Override
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		TextView textView = new TextView(this);
+		textView.setText("Following");
+	}
+}
---
+++ src/com/example/darkroom/UploadsActivity.java
@@ -0,0 +1,14 @@
+package com.example.darkroom;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class UploadsActivity extends Activity{
+	@Override
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		TextView textView = new TextView(this);
+		textView.setText("Uploads");
+	}
+}
