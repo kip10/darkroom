b9955fa87dbdcb580ed31703c00f4a34c4be22ba
Eric Klawitter <eklawitter1@comcast.net>
2014-04-22 16:03:25 -0400

    ReviewsActivity implemented correctly using the ReviewsObject

--- src/com/example/darkroom/ReviewsActivity.java
+++ src/com/example/darkroom/ReviewsActivity.java
@@ -1,16 +1,20 @@
 package com.example.darkroom;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
 import android.content.Intent;
-import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
+import com.example.listObjects.ReviewsObject;
 
 public class ReviewsActivity extends Activity{
 	private Button backButton;
@@ -20,7 +24,7 @@ public class ReviewsActivity extends Activity{
 	private TextView userName;
 	private TextView score;
 	private CheckBox checkBoxFollowing;
-	
+	private List<ReviewsObject> reviewList = new ArrayList<ReviewsObject>();
 	 
 	@Override
 	public void onCreate(Bundle savedInstanceState){
@@ -66,6 +70,8 @@ public class ReviewsActivity extends Activity{
 		
 		score = (TextView)findViewById(R.id.reviewsScore);
 		populateScore();
+		
+		populateListView();
 	}
 	private void populateScore() {
 		// TODO Auto-generated method stub
@@ -104,5 +110,67 @@ public class ReviewsActivity extends Activity{
 				0);
 		
 		*/
+	}
+	private void populateListView() {
+		ArrayAdapter<ReviewsObject> myAdapter = new MyListAdapter();
+		ListView rList = (ListView) findViewById(R.layout.activity_reviews);
+		rList.setAdapter(myAdapter);
+		
+	}
+	
+	private class MyListAdapter extends ArrayAdapter<ReviewsObject>{
+		public MyListAdapter(){
+			super(ReviewsActivity.this, R.layout.reviews_layout, reviewList);
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			//Make sure we have view to work with
+			View itemView = convertView;
+			if(itemView==null){
+				itemView = getLayoutInflater().inflate(R.layout.reviews_layout, parent, false);
+			}
+			ReviewsObject currentObject = reviewList.get(position);
+			ImageView avatar = (ImageView)itemView.findViewById(R.id.pageReviewAvatar);
+			//TODO: set image
+			//TODO: set onclick listener
+			
+			ImageView image = (ImageView)itemView.findViewById(R.id.pageReivewImage);
+			//TODO: set image
+			//TODO: set on click listneer
+			
+			TextView review = (TextView)itemView.findViewById(R.id.pageReviewReview);
+			review.setText(currentObject.getReview());
+
+			TextView username = (TextView)itemView.findViewById(R.id.pageReviewUser);
+			username.setText(currentObject.getUserName());
+			username.setOnClickListener(new View.OnClickListener() {
+				
+				@Override
+				public void onClick(View v) {
+					Intent goToUserPage = new Intent(v.getContext(),
+							UserHomeActivity.class);
+					startActivityForResult(goToUserPage, 0);
+					
+				}
+			});
+			
+			TextView rating0 = (TextView)itemView.findViewById(R.id.pageReviewScore0);
+			rating0.setText(currentObject.getRating1());
+			
+			TextView rating1 = (TextView)itemView.findViewById(R.id.pageReviewScore1);
+			rating1.setText(currentObject.getRating2());
+			
+			TextView rating2 = (TextView)itemView.findViewById(R.id.pageReviewScore2);
+			rating2.setText(currentObject.getRating3());
+			
+			TextView rating3 = (TextView)itemView.findViewById(R.id.pageReviewScore3);
+			rating3.setText(currentObject.getRating4());
+			
+			TextView rating4 = (TextView)itemView.findViewById(R.id.pageReviewScore4);
+			rating4.setText(currentObject.getRating5());
+			
+			return itemView;
+		}
 	}
 }
