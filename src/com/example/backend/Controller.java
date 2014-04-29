b121756c7d5c5a72c5060cd069b3901f0d6d3c6b
Eric Klawitter <eklawitter1@comcast.net>
2014-04-28 20:35:59 -0400

    made backend package for convenience

---
+++ src/com/example/backend/Controller.java
@@ -0,0 +1,233 @@
+package com.example.backend;
+
+import java.io.File;
+
+public class Controller {
+
+	private static String userName;
+	private static String userAvatarPath;
+	private static String equipmentText;
+	private static String styleText;
+	private static String websiteText;
+	private static String workText;
+	private static String aboutText;
+	
+	private Boolean forHire; //need to translate 1s and 0s to boolean
+	private Boolean reviewsAllowed;
+	private Boolean guestBookAllowed;
+	private Boolean commentsAllowed;
+	
+	
+	private static String photoTitle;
+	private static String rating;
+	private static String photoPath;
+	
+	
+	public static String getUserName() {
+		String data;
+		if (userName == null) {
+			data = DatabaseQueryer.getData("userName");
+			//parse
+			//set userName
+			return userName;
+		}
+		return userName;
+	}
+	public static void setUserName(String userName) {
+		Controller.userName = userName;
+	}
+	
+	public static String getUserAvatarPath() {
+		String data;
+		if (userAvatarPath == null) {
+			data = DatabaseQueryer.getData("userAvatarPath");
+			//parse
+			//set userAvatarPath
+			return userAvatarPath;
+		}
+		return userAvatarPath;
+	}
+	public static void setUserAvatar(String userAvatarPath) {
+		Controller.userAvatarPath = userAvatarPath;
+	}
+	
+	public static String getEquipmentText() {
+		String data;
+		if (equipmentText == null) {
+			data = DatabaseQueryer.getData("equipmentText");
+			//parse
+			//set equipmentText
+			return equipmentText;
+		}
+		return equipmentText;
+	}
+	public static void setEquipmentText(String equipmentText) {
+		Controller.equipmentText = equipmentText;
+	}
+	
+	public static String getStyleText() {
+		String data;
+		if (styleText == null) {
+			data = DatabaseQueryer.getData("styleText");
+			//parse
+			//set styleText
+			return styleText;
+		}
+		return styleText;
+	}
+	public static void setStyleText(String styleText) {
+		Controller.styleText = styleText;
+	}
+	
+	public static String getWebsiteText() {
+		String data;
+		if (websiteText == null) {
+			data = DatabaseQueryer.getData("websiteText");
+			//parse
+			//set styleText
+			return websiteText;
+		}
+		return websiteText;
+	}
+	public static void setWebsiteText(String websiteText) {
+		Controller.websiteText = websiteText;
+	}
+	
+	public static String getWorkText() {
+		String data;
+		if (workText == null) {
+			data = DatabaseQueryer.getData("workText");
+			//parse
+			//set workText
+			return workText;
+		}
+		return workText;
+	}
+	public static void setWorkText(String workText) {
+		Controller.workText = workText;
+	}
+	
+	public static String getAboutText() {
+		String data;
+		if (aboutText == null) {
+			data = DatabaseQueryer.getData("aboutText");
+			//parse
+			//set aboutText
+			return aboutText;
+		}
+		return aboutText;
+	}
+	public static void setAboutText(String aboutText) {
+		Controller.aboutText = aboutText;
+	}
+	
+	public Boolean getForHire() {
+		String data;
+		if (forHire == null) {
+			data = DatabaseQueryer.getData("forHire");
+			//parse
+			//set forHire
+			return forHire;
+		}
+		return forHire;
+	}
+	public void setForHire(Boolean forHire) {
+		this.forHire = forHire;
+	}
+	
+	public Boolean getReviewsAllowed() {
+		String data;
+		if (reviewsAllowed == null) {
+			data = DatabaseQueryer.getData("reviewsAllowed");
+			//parse
+			//set reviewsAllowed
+			return reviewsAllowed;
+		}
+		return reviewsAllowed;
+	}
+	public void setReviewsAllowed(Boolean reviewsAllowed) {
+		this.reviewsAllowed = reviewsAllowed;
+	}
+	
+	public Boolean getGuestBookAllowed() {
+		String data;
+		if (guestBookAllowed == null) {
+			data = DatabaseQueryer.getData("guestBookAllowed");
+			//parse
+			//set guestBookAllowed
+			return guestBookAllowed;
+		}
+		return guestBookAllowed;
+	}
+	public void setGuestBookAllowed(Boolean guestBookAllowed) {
+		this.guestBookAllowed = guestBookAllowed;
+	}
+	
+	public Boolean getCommentsAllowed() {
+		String data;
+		if (commentsAllowed == null) {
+			data = DatabaseQueryer.getData("commentsAllowed");
+			//parse
+			//set forHire
+			return commentsAllowed;
+		}
+		return commentsAllowed;
+	}
+	public void setCommentsAllowed(Boolean commentsAllowed) {
+		this.commentsAllowed = commentsAllowed;
+	}
+	
+	public static String getPhotoTitle() {
+		String data;
+		if (photoTitle == null) {
+			data = DatabaseQueryer.getData("photoTitle");
+			//parse
+			//set forHire
+			return photoTitle;
+		}
+		return photoTitle;
+	}
+	public static void setPhotoTitle(String photoTitle) {
+		
+		Controller.photoTitle = photoTitle;
+	}
+	
+	public static String getRating() {
+		String data;
+		if (rating == null) {
+			data = DatabaseQueryer.getData("rating");
+			//parse
+			//set rating
+			return rating;
+		}
+		return rating;
+	}
+	public static void setRating(String rating) {
+		Controller.rating = rating;
+	}
+	
+	public static String getPhotoPath() {
+		String data;
+		if (photoPath == null) {
+			data = DatabaseQueryer.getData("photoPath");
+			//parse
+			//set PhotoPath
+			return photoPath;
+		}
+		return photoPath;
+	}
+	public static void setPhoto(File photo) {
+		Controller.photoPath = photoPath;
+	}
+	
+	
+
+
+	
+
+	
+
+
+
+
+}
---
+++ src/com/example/backend/DatabaseQueryer.java
@@ -0,0 +1,129 @@
+
+package com.example.backend;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.util.Log;
+
+public class DatabaseQueryer {
+
+	/**
+	 * Function to query the database and obtain a result set in the form of a
+	 * JSON array.
+	 * 
+	 * @param phpFileName
+	 *            -- Name of the php file in the EECS395 folder -- note: the
+	 *            method accepts files using JUST THE NAME of the php file-- ie
+	 *            to use myFile.php, use argument "myFile";
+	 * @return JSONArray containing the results of the query.
+	 * @throws JSONException -- if there is an error parsing data to JSONArray object
+	 */
+	public static String getData(String phpFileName) {
+		String result = "";
+		InputStream isr = null;
+		try {
+			HttpClient httpclient = new DefaultHttpClient();
+			HttpPost httppost = new HttpPost(
+					"http://10.0.2.2/EECS395/"+phpFileName+ ".php");
+			HttpResponse response = httpclient.execute(httppost);
+			
+			HttpEntity entity = response.getEntity();
+			isr = entity.getContent();
+		} catch (Exception e) {
+			Log.e("log_tag", "Error in http connection " + e.toString());
+		}
+		// convert response to string
+		try {
+			BufferedReader reader = new BufferedReader(new InputStreamReader(
+					isr, "iso-8859-1"), 8);
+			StringBuilder sb = new StringBuilder();
+			String line = null;
+			while ((line = reader.readLine()) != null) {
+				sb.append(line + "\n");
+			}
+			isr.close();
+
+			result = sb.toString();
+		} catch (Exception e) {
+			Log.e("log_tag", "Error  converting result " + e.toString());
+		}
+		/*
+		 * // parse json data try { String s = ""; JSONArray jArray = new
+		 * JSONArray(result);
+		 * 
+		 * for (int i = 0; i < jArray.length(); i++) { JSONObject json =
+		 * jArray.getJSONObject(i); s = s + "Name : " +
+		 * json.getString("username") + " " + json.getString("email"); }
+		 * 
+		 * 
+		 * 
+		 * } catch (Exception e) {
+		 * 
+		 * Log.e("log_tag", "Error Parsing Data " + e.toString()); }
+		 */
+		return result;
+
+	}
+
+	public void gay() throws IOException{
+		URL url = new URL("http://localhost/CD/user/test");
+	    String result = "";
+	    String data = "fName=" + URLEncoder.encode("Atli", "UTF-8");
+	    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+	    try {
+
+	        connection.setDoInput(true);
+	        connection.setDoOutput(true);
+	        connection.setUseCaches(false);
+	        connection.setRequestMethod("POST");
+	        connection.setRequestProperty("Content-Type",
+	                "application/x-www-form-urlencoded");
+
+	        // Send the POST data
+	        DataOutputStream dataOut = new DataOutputStream(
+	                connection.getOutputStream());
+	        dataOut.writeBytes(data);
+	        dataOut.flush();
+	        dataOut.close();
+
+	        BufferedReader in = null;
+	        try {
+	            String line;
+	            in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+	            while ((line = in.readLine()) != null) {
+	                result += line;
+	            }
+	        } finally {
+	            if (in != null) {
+	                in.close();
+	            }
+	        }
+	        String g;
+	        while ((g = in.readLine()) != null) {
+	            result += g;
+	        }
+	        in.close();
+
+	    } finally {
+	        connection.disconnect();
+	        System.out.println(result);
+	    }
+	}
+}
+
